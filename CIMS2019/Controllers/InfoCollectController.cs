d5ab1040b6e61f6f6370a50dfaec22051e850b67
rexfa <rex_fa@163.com>
2019-09-09 16:38:46 +0800

    Start

---
+++ CIMS2019/Controllers/InfoCollectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIMS2019.Controllers
+{
+    public class InfoCollectController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index(string str)
+        {
+            return RedirectToPage("http://u.vivatech.cn/h/6601");
+        }
+    }
+}
