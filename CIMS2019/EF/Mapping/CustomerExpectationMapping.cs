fddf64fccd48b46e08ba6666b6ad112581147710
rexfa <rex_fa@hotmail.com>
2019-09-10 23:24:41 +0800

    测试

--- CIMS2019/EF/Mapping/CustomerExpectationMapping.cs
+++ CIMS2019/EF/Mapping/CustomerExpectationMapping.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CIMS2019.EF.Domain;
 
 namespace CIMS2019.EF.Mapping
 {
-    public class CustomerExpectationMapping
+    public class CustomerExpectationMapping : IEntityTypeConfiguration<CustomerExpectation>
     {
+        public void Configure(EntityTypeBuilder<CustomerExpectation> builder)
+        {
+            builder.ToTable("CustomerExpectation");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.IDNumber).IsRequired().HasMaxLength(20);
+            builder.Property(x => x.PhoneNumber).IsRequired().HasMaxLength(20);
+            builder.HasIndex(x => x.LoginName).IsUnique();
+            builder.Property(x => x.LoginName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Password).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Salt).IsRequired().HasDefaultValue("").HasMaxLength(10);
+            builder.Property(x => x.Avatar).HasMaxLength(200);
+            builder.Property(x => x.Fever).IsRequired().HasDefaultValue(0);
+            builder.Property(x => x.CreatedOn).IsRequired();
+            builder.Property(x => x.CustomerName).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.IsDeleted).IsRequired().HasDefaultValue(false);
+            //builder.Ignore(x => x.Blogs);
+            //builder.HasMany(x => x.Blogs).WithOne(x => x.Customer).HasForeignKey(x => x.CustomerId).OnDelete(DeleteBehavior.Restrict);
+            //builder.Ignore(x => x.Pictures);
+            //builder.HasMany(x => x.Pictures).WithOne(x => x.Customer).HasForeignKey(x => x.CustomerId).OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- CIMS2019/EF/Mapping/CustomerMapping.cs
+++ CIMS2019/EF/Mapping/CustomerMapping.cs
@@ -10,11 +10,14 @@ namespace CIMS2019.EF.Mapping
         {
             builder.ToTable("Customer");
             builder.HasKey(x => x.Id);
-            builder.HasIndex(x => x.Name).IsUnique();
-            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.IDNumber).IsRequired().HasMaxLength(20);
+            builder.Property(x => x.PhoneNumber).IsRequired().HasMaxLength(20);
+            builder.HasIndex(x => x.LoginName).IsUnique();
+            builder.Property(x => x.LoginName).IsRequired().HasMaxLength(100);
             builder.Property(x => x.Password).IsRequired().HasMaxLength(50);
             builder.Property(x => x.Salt).IsRequired().HasDefaultValue("").HasMaxLength(10);
             builder.Property(x => x.Avatar).HasMaxLength(200);
+            builder.Property(x => x.Fever).IsRequired().HasDefaultValue(0);
             builder.Property(x => x.CreatedOn).IsRequired();
             builder.Property(x => x.CustomerName).IsRequired().HasMaxLength(50);
             builder.Property(x => x.IsDeleted).IsRequired().HasDefaultValue(false);
